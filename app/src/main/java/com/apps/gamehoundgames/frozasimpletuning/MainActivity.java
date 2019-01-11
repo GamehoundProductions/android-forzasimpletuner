c793411779ce12f67883f3d8e273ead00c385143
Bender <zach.bender@protonmail.com>
2019-01-10 22:23:55 -0700

    V1.1 release

--- app/src/main/java/com/apps/gamehoundgames/frozasimpletuning/MainActivity.java
+++ app/src/main/java/com/apps/gamehoundgames/frozasimpletuning/MainActivity.java
@@ -1,11 +1,11 @@
 package com.apps.gamehoundgames.frozasimpletuning;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.apps.gamehoundgames.frozasimpletuning.InputFields.EventDelegate;
